5474c2e05bcffecc187c220754dc85421be00ebd
BartnikLukasz <bartnik.luk@gmail.com>
2020-12-14 12:02:50 +0100

    Added response bodies for adding book, getting all books and getting one book.

--- management/src/main/java/library/system/management/Controller.java
+++ management/src/main/java/library/system/management/Controller.java
@@ -1,13 +1,15 @@
 package library.system.management;
 
+import library.system.management.models.Book;
 import library.system.management.models.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/library")
+@RequestMapping(path="/library")
 public class Controller {
 
     @Autowired
@@ -16,6 +18,25 @@ public class Controller {
     @Autowired
     RestTemplate restTemplate;
 
+    @PostMapping(path="/add")
+    public @ResponseBody void addBook(@RequestParam String title, @RequestParam String author, @RequestParam String genre){
+        Book tempBook = new Book();
+        tempBook.setTitle(title);
+        tempBook.setAuthor(author);
+        tempBook.setGenre(genre);
 
+        bookRepository.save(tempBook);
+    }
+
+    @GetMapping(path="/getAll")
+    public @ResponseBody Iterable<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    @GetMapping(path="/getBook/{bookId}")
+    public @ResponseBody Book getBook(@PathVariable("bookId") String bookId){
+        Optional<Book> tempBook = bookRepository.findById(Integer.parseInt(bookId));
+        return tempBook.orElse(null);
+    }
 
 }
